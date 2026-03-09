5883617aee014aa997e0b86b562ec0193ca4c174
THOMAS-Tunn <thienthomas369@gmail.com>
2026-03-09 12:12:44 -0400

    Update App.tsx

--- grocery-community-web/src/App.tsx
+++ grocery-community-web/src/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
   useEffect(() => {
     document.title = "Ten Shop";
 
-    const iconUrl = "https://ik.imagekit.io/0b1iirbdi/logo-modified.png";
+    const iconUrl = "https://ik.imagekit.io/0b1iirbdi/logo-modified.png?updatedAt=1773072541949";
 
     let link = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
 
